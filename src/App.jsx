a07dc90b5d81fa095dce1c7c72c1e190a63a037d
NIGGERTRON\Parthesh <parthesh01@gmail.com>
2025-10-31 13:46:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
+import ServiceSummary from './sections/ServiceSummary'
+import Services from './sections/Services'
+import ReactLenis from 'lenis/react'
+import About from './sections/About'
+import Works from './sections/Works'
+import ContactSummary from './sections/ContactSummary'
+import Contact from './sections/Contact'
+
+const App = () => {
+  return (
+    <ReactLenis root className='relative w-screen min-h-screen overflow-x-hidden'>
+      <Navbar />
+      <Hero />
+      <ServiceSummary />
+      <Services />
+      <About/>
+      <Works/>
+      <ContactSummary/>
+      <Contact/>
+    </ReactLenis>
+  )
+}
+
+export default App
---
+++ src/sections/About.jsx
@@ -0,0 +1,65 @@
+
+import { useRef } from "react";
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
+import { AnimatedTextLines } from "../components/AnimatedTextLine";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const About = () => {
+  const text = `Passionate about clean architecture
+    I build scalable, high-performance solutions
+    from prototype to production`;
+  const aboutText = `Obsessed with crafting seamless digital experiences—from responsive, elegant React interfaces to scalable serverless backends. Every project I touch blends precision, creativity, and purpose.
+
+When I’m not shipping:
+⚡️ Designing intuitive, lightning-fast front-ends that users love
+🌍 Transforming complex ideas into clean, engaging interactions
+🧠 Leveling up daily—because innovation never sleeps
+🎶 Building and debugging to the rhythm of synthwave and caffeine`;
+  const imgRef = useRef(null);
+  useGSAP(() => {
+    gsap.to("#about", {
+      scale: 0.95,
+      scrollTrigger: {
+        trigger: "#about",
+        start: "bottom 80%",
+        end: "bottom 20%",
+        scrub: true,
+        markers: false,
+      },
+      ease: "power1.inOut",
+    });
+
+    gsap.set(imgRef.current, {
+      clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
+    });
+    gsap.to(imgRef.current, {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      duration: 2,
+      ease: "power4.out",
+      scrollTrigger: { trigger: imgRef.current },
+    });
+  });
+  return (
+    <section id="about" className="min-h-screen bg-black rounded-b-4xl">
+      <AnimatedHeaderSection
+        subTitle={"Cod with purpose, Built to scale"}
+        title={"About"}
+        text={text}
+        textColor={"text-white"}
+        withScrollTrigger={true}
+      />
+      <div className="flex flex-col items-center justify-between gap-16 px-10 pb-16 text-xl font-light tracking-wide lg:flex-row md:text-2xl lg:text-3xl text-white/60">
+        <img
+          ref={imgRef}
+          src="images/man1.jpg"
+          alt="man"
+          className="w-md rounded-3xl"
+        />
+        <AnimatedTextLines text={aboutText} className={"w-full"} />
+      </div>
+    </section>
+  );
+};
+
+export default About;
